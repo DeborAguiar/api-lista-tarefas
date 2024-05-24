b893bdec0a2a305adf90afbbcdbe0117be51653f
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2024-05-23 21:30:47 -0300

    add .env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express()
 app.use(express.json())
-const port   = 3000
+const port = process.env.PORT || 3000;
 mongoose.connect("mongodb+srv://admin:admin@tasks.xxe6qrh.mongodb.net/?retryWrites=true&w=majority&appName=Tasks");
 
 const Task  = mongoose.model('Task', {
